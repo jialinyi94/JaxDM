278ead8ef221859915f4a7a6ce2bff454f822e0e
jialinyi <jason.yijialin@gmail.com>
2024-03-17 11:39:02 +0000

    add doc

--- jaxdm/bandit/adversarial.py
+++ jaxdm/bandit/adversarial.py
@@ -6,11 +6,32 @@ import jax.numpy as jnp
 
 class Exp3_IX:
     def __init__(self, n_arms: int, learning_rate: Union[float, Generator]) -> None:
+        """Exp3-IX algorithm from 
+            Algorithm 10 in https://tor-lattimore.com/downloads/book/book.pdf.
+
+        Parameters
+        ----------
+        n_arms : int
+            number of arms.
+        learning_rate : Union[float, Generator]
+            a fixed learning rate or scheduler.
+        """
         self.n_arms = n_arms
         self.sel_probs = jnp.ones(n_arms) / n_arms
         self.learning_rate = learning_rate
 
     def update(self, selected: int, reward: float, lr: float):
+        """One step update using importance weighted estimator.
+
+        Parameters
+        ----------
+        selected : int
+            the selected arm id.
+        reward : float
+            a reward between [0, 1].
+        lr : float
+            the learning rate value.
+        """
         assert 0 <= reward <= 1
         loss_est = (1-reward) / (self.sel_probs[selected] + lr/2)
         factor = jnp.exp(-lr * loss_est)
@@ -18,6 +39,18 @@ class Exp3_IX:
         self.sel_probs = self.sel_probs / self.sel_probs.sum()
 
     def action(self, observations: tuple[int, float]):
+        """Return the selection probability on arms.
+
+        Parameters
+        ----------
+        observations : tuple[int, float]
+            (arm_id, reward)
+
+        Returns
+        -------
+        Array
+            a selection probability array
+        """
         if isinstance(self.learning_rate, float):
             lr = self.learning_rate
         else:
@@ -33,9 +66,11 @@ if __name__ == "__main__":
         1: 0.1
     }
     action_space = jnp.array(list(experiences.keys()))
-    agent = Exp3_IX(2, learning_rate=0.1)
+
+    from jaxdm.opt.learning_rate import exponent_scheduler
+    agent = Exp3_IX(2, learning_rate=exponent_scheduler(0.1))
     action = 0
-    for i in range(100):
+    for i in range(1000):
         reward = experiences[action]
         probs = agent.action((action, reward))
         rng, subkey = jax.random.split(rng)
--- jaxdm/opt/learning_rate.py
+++ jaxdm/opt/learning_rate.py
@@ -1,6 +1,20 @@
 def exponent_scheduler(init_value: float, exponent: float = 1/2):
+    """Learning rate decays according to a power function on the steps.
+
+    Parameters
+    ----------
+    init_value : float
+        initial learning rate.
+    exponent : float, optional
+        the order of exponent, by default 1/2
+
+    Yields
+    ------
+    float
+        the learning rate
+    """
     step = 1
     while True:
-        lr = step / (init_value) ** (exponent)
+        lr = init_value / (step) ** (exponent)
         yield lr
         step += 1
